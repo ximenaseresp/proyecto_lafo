7f563a4c609d7a66f14498dbce86fab54fdee0c2
ximenaseresp <48834023+ximenaseresp@users.noreply.github.com>
2019-05-15 22:01:21 -0500

    news

---
+++ news.js
@@ -0,0 +1,11 @@
+  $(document).ready(function() {
+  $("#carousel").owlCarousel({
+      navigation : false,
+      slideSpeed : 500,
+      paginationSpeed : 800,
+      rewindSpeed : 1000,
+      singleItem: true,
+      autoPlay : true,
+      stopOnHover : true,
+  });
+});
