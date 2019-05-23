29446febca172bfe56aba9e8dcd4c2fddfadd7ef
ximenaseresp <48834023+ximenaseresp@users.noreply.github.com>
2019-05-23 01:58:14 -0500

    RESIDENCIA

--- galeria.js
+++ galeria.js
@@ -1,35 +1,27 @@
-$(document).ready(function() {
-        
-   /* activate the carousel */
-   $("#modal-carousel").carousel({interval:false});
-
-   /* change modal title when slide changes */
-   $("#modal-carousel").on("slid.bs.carousel",       function () {
-        $(".modal-title")
-        .html($(this)
-        .find(".active img")
-        .attr("title"));
-   });
-
-   /* when clicking a thumbnail */
-   $(".row .thumbnail").click(function(){
-    var content = $(".carousel-inner");
-    var title = $(".modal-title");
-  
-    content.empty();  
-    title.empty();
-  
-  	var id = this.id;  
-     var repo = $("#img-repo .item");
-     var repoCopy = repo.filter("#" + id).clone();
-     var active = repoCopy.first();
-  
-    active.addClass("active");
-    title.html(active.find("img").attr("title"));
-  	content.append(repoCopy);
-
-    // show the modal
-  	$("#modal-gallery").modal("show");
-  });
-
+    $(window).load(function(){
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
 });
